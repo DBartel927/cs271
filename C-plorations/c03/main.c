6982c100c18fc34272d0ef3e5a24f03e813ba7b8
Declan Bartel <barteld@oregonstate.edu>
2024-10-27 14:51:14 -0700

    Commit 10/27/2024

--- C-plorations/c03/main.c
+++ C-plorations/c03/main.c
@@ -1,12 +1,45 @@
 /****************************************
  * C-ploration 3 for CS 271
  * 
- * [NAME] $YOUR_NAME$
- * [TERM] FALL $YEAR$
+ * [NAME] $Declan Bartel$
+ * [TERM] FALL $2024$
  * 
  ****************************************/
  
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LINE_LENGTH 200
+ 
+int main(int argc, const char *argv[])
 {	
-	return 1;
+// if incorrect number of arguments
+if (argc != 2) {
+	printf("Usage: %s [filename]\n", argv[0]);
+	exit(EXIT_FAILURE);
+}
+FILE *fin = fopen(argv[1], "r");
+if (fin == NULL) {
+	perror("Error opening file");
+	exit(EXIT_FAILURE);
+}
+char out_file[200];
+strcpy(out_file, argv[1]);
+strcat(out_file, ".echo");
+	
+FILE *fout = fopen(out_file, "w+");
+if (fout == NULL) {
+	perror("Error opening output file");
+	exit(EXIT_FAILURE);
+}
+char line[MAX_LINE_LENGTH] = {0};
+unsigned int line_num = 0;
+while (fgets(line, sizeof(line), fin)) {
+	line_num++;
+	printf("[%04d] %s", line_num, line);
+	fprintf(fout, "%s", line);
+}
+fclose(fin);
+fclose(fout);
+return 1;
 }
